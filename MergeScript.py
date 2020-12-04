e80836f1b61a698c547878065810dd8e4b0a8ec5
Maik Wydra <m.wydra0206@gmail.com>
2020-12-04 16:20:48 +0100

    Updated Dataset with Intesive Beds First Commit

--- MergeScript.py
+++ MergeScript.py
@@ -3,18 +3,13 @@ import numpy as np
 import os
 
 #Read the CSV, which contains the Number of Beds and Infected
-<<<<<<< HEAD
 bedsAndInfected=pd.read_csv("./data/intensivData/bedsAndInfected.csv", parse_dates=["date"],dayfirst=False)
-=======
-bedsAndInfected=pd.read_csv("./data/intensivData/bedsAndInfected.csv", parse_dates=["date"],dayfirst=True)
->>>>>>> ea15ed42ab7e376f176f7214c31b329ec4516d93
 
 #the attribute "date" is in datetime. I want to merge the data based on the date in the attribute "date"
 bedsAndInfected['just_date']=pd.to_datetime(bedsAndInfected['date'], errors="ignore", utc=True).dt.date
 bedsAndInfected=bedsAndInfected.drop(columns=['date'])
 
 #parse the rki dataset
-<<<<<<< HEAD
 covid19Updated = pd.read_csv("./data/rki_covid19_04_12_2020.csv", parse_dates=["Meldedatum", "Refdatum"], dayfirst=False, index_col="ObjectId")
 covid19 = pd.read_csv("./data/rki_covid19_19_10_2020.csv", parse_dates=["Meldedatum", "Refdatum"], dayfirst=False, index_col="ObjectId")
 
@@ -22,29 +17,14 @@ covid19 = pd.read_csv("./data/rki_covid19_19_10_2020.csv", parse_dates=["Meldeda
 #do the same transformation from datetime to date with "refdatum" and call the new attribute just_date
 covid19['just_date']=pd.to_datetime(covid19['Meldedatum'], errors="ignore", utc=True).dt.date
 covid19Updated['just_date']=pd.to_datetime(covid19Updated['Meldedatum'], errors="ignore", utc=True).dt.date
-=======
-covid19 = pd.read_csv("./data/rki_covid19_19_10_2020.csv", parse_dates=["Meldedatum", "Refdatum"], dayfirst=True, index_col="ObjectId")
-
-#do the same transformation from datetime to date with "refdatum" and call the new attribute just_date
-covid19['just_date']=pd.to_datetime(covid19['Refdatum'], errors="ignore", utc=True).dt.date
->>>>>>> ea15ed42ab7e376f176f7214c31b329ec4516d93
 
 #merge the data, where just_date is the same and the IdBundesland
 #I used "left" so that to the rki data the correct data from bedsAndInfected will be added
 covid19=pd.merge(covid19, bedsAndInfected, how='left', on=['just_date','IdBundesland'])
 covid19=covid19.drop(columns=['just_date'])
-<<<<<<< HEAD
 
 covid19Updated=pd.merge(covid19Updated, bedsAndInfected, how='left', on=['just_date','IdBundesland'])
 covid19Updated=covid19Updated.drop(columns=['just_date'])
 
-
-
 covid19.to_csv(r'./data/intensivData/newRKI.csv', index= False, header=True)
 covid19Updated.to_csv(r'./data/intensivData/newRkiUpdated.csv', index= False, header=True)
-=======
-print(covid19.head())
-
-
-covid19.to_csv(r'./data/intensivData/newRKI.csv', index= False, header=True)
->>>>>>> ea15ed42ab7e376f176f7214c31b329ec4516d93
